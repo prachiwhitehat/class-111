ef069652ac8aa40dd176b7857bcb50b45f6a5b36
prachiwhitehat <71639231+prachiwhitehat@users.noreply.github.com>
2021-12-02 23:36:00 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,19 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("studentMarks.csv")
+data = df["Math_score"].tolist()
+
+#plotting the graph 
+fig = ff.create_distplot([data],["Math Scores"], show_hist= False)
+fig.show()
+
+#calculating the mean and standard deviation of the population data
+mean = statistics.mean(data)
+std_deviation = statistics.stdev(data)
+print("mean of population:- ",mean)
+print("Standard deviation of popultion:- ",std_deviation)
